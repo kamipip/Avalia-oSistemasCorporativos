2049b381ab70f619cf41370c85489385b64ab4f0
KAMILLA AUGUSTO NUNES <20221148060028@ifrn.local>
2023-12-27 08:11:20 -0300

    first commit

---
+++ src/main/java/com/hospedes/demo/HospedesApplication.java
@@ -0,0 +1,13 @@
+package com.hospedes.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospedesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospedesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospedes/demo/HospedesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospedes.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospedesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
