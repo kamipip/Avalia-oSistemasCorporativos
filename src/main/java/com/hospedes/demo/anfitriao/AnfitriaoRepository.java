8e29f6073ab36d245a0e8669f723cf966f75653b
KAMILLA AUGUSTO NUNES <20221148060028@ifrn.local>
2023-12-27 11:10:47 -0300

    hospede

--- src/main/java/com/hospedes/demo/anfitriao/AnfitriaoRepository.java
+++ src/main/java/com/hospedes/demo/anfitriao/AnfitriaoRepository.java
@@ -4,5 +4,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnfitriaoRepository extends JpaRepository<Anfitriao, Long> {
-    // adicionar métodos personalizados aqui
+    // adicionar métodos 
 }
